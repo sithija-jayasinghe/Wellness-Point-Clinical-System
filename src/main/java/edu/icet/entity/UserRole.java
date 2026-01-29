c2caba177c62a5cf0b7b24f84aaadd6d07130359
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-29 14:05:49 +0530

    implement User, Clinic, and Role entities
    - Added Clinic.java for branch management
    - Added User.java and Role.java for authentication
    - Implemented UserClinic and UserRole for many-to-many mappings
    - Configured JPA annotations and Lombok

---
+++ src/main/java/edu/icet/entity/UserRole.java
@@ -0,0 +1,22 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user_role")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userRoleId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
