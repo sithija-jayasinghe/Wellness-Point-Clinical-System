79e69dfe33385092a4bffaec8277af980b3613ce
sithija-jayasinghe <harshanajayasinghe113@gmail.com>
2026-02-05 18:19:01 +0530

    fix: update allowed origin for CORS configuration from localhost:5174 to localhost:5173

--- src/main/java/edu/icet/config/SecurityConfig.java
+++ src/main/java/edu/icet/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(List.of("http://localhost:5174"));
+                    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
                     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowedHeaders(List.of("*"));
                     return configuration;
